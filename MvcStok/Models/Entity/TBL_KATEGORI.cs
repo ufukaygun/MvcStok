6f97fc8f0e3838003ed5c8168ab8d1fb4a041260
ufukaygun <u.aygun869@gmail.com>
2024-04-10 23:06:18 +0300

    Kategori Ekle için Validation Required işlemi yapıldı

--- MvcStok/Models/Entity/TBL_KATEGORI.cs
+++ MvcStok/Models/Entity/TBL_KATEGORI.cs
@@ -11,7 +11,8 @@ namespace MvcStok.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class TBL_KATEGORI
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,8 @@ namespace MvcStok.Models.Entity
         }
     
         public short KATEGORIID { get; set; }
+
+        [Required]
         public string KATEGORIAD { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
