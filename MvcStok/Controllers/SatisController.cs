0827d65826df4cb160d777ac89fbaa7018cd111d
ufukaygun <u.aygun869@gmail.com>
2024-04-26 18:47:25 +0300

    Popup Modal üzerine Satış işlemi eklendi

--- MvcStok/Controllers/SatisController.cs
+++ MvcStok/Controllers/SatisController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcStok.Models.Entity;
+using MvcStok.Controllers;
 
 namespace MvcStok.Controllers
 {
     public class SatisController : Controller
     {
         // GET: Satis
+        MvcDbStokEntities db = new MvcDbStokEntities();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBL_SATISLAR p)
+        {
+            db.TBL_SATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+
     }
 }
